7692b4493665cc112d49d16543ee6e3600e4aa3a
Keith Randall <khr@golang.org>
2015-10-31 22:49:36 -0700

    More benchmark tests
    Change-Id: Ib2a815f275ab71f20a041d7f8c3a8e67e6cad410

--- memmove_test.go
+++ memmove_test.go
@@ -80,18 +80,28 @@ func benchMemMove(b *testing.B, f func(dst, src *[2048]byte)) {
 }
 
 // Results:
-// 3.7 GHz Quad-Core Intel Xeon E5
+// 3.7 GHz Quad-Core Intel Xeon E5 (mac pro)
 // BenchmarkMemMove-8    	20000000	        77.5 ns/op	26413.79 MB/s
 // BenchmarkMemMoveSSE2-8	50000000	        37.1 ns/op	55234.76 MB/s
 // BenchmarkMemMoveAVX-8 	50000000	        37.1 ns/op	55229.94 MB/s
 // (prefetch doesn't help, aligned reads/writes don't help)
 
-// Intel(R) Core(TM) i7-4600U CPU @ 2.10GHz
+// Intel(R) Core(TM) i7-4600U CPU @ 2.10GHz (HP laptop)
 // BenchmarkMemMove-4    	20000000	        85.1 ns/op	24056.98 MB/s
 // BenchmarkMemMoveSSE2-4	30000000	        44.4 ns/op	46176.00 MB/s
 // BenchmarkMemMoveAVX-4 	50000000	        24.3 ns/op	84248.36 MB/s
 
-// Intel(R) Xeon(R) CPU E5-1650 0 @ 3.20GHz
+// Intel(R) Xeon(R) CPU E5-1650 0 @ 3.20GHz (work desktop)
 // BenchmarkMemMove-12     20000000                84.1 ns/op      24357.54 MB/s
 // BenchmarkMemMoveSSE2-12 30000000                37.5 ns/op      54555.20 MB/s
 // BenchmarkMemMoveAVX-12  30000000                38.2 ns/op      53670.40 MB/s
+
+// Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz  (amazon ec2)
+// BenchmarkMemMove-2     20000000       100 ns/op   20440.44 MB/s
+// BenchmarkMemMoveSSE2-2 30000000        52.5 ns/op 38990.39 MB/s
+// BenchmarkMemMoveAVX-2  50000000        29.0 ns/op 70503.32 MB/s
+
+// AMD Opteron(tm) Processor 4122 (dreamhost)
+// BenchmarkMemMove-4		 5000000	       387 ns/op	5285.22 MB/s
+// BenchmarkMemMoveSSE2-4	 3000000	       427 ns/op	4787.95 MB/s
+// no AVX instructions
